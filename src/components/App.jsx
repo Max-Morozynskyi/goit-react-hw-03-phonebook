09b018e2216ad470a1b77a80afc7154284f96c42
Max-Morozynskyi <m0r0zin@ukr.net>
2023-02-05 16:20:29 +0200

    clear state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import { Filter } from './Filter/Filter';
 
 export class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Bart Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Weasley', number: '443-89-12' },
-      { id: 'id-3', name: 'Eric Cartman', number: '645-17-79' },
-      { id: 'id-4', name: 'Porkey Pig', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
